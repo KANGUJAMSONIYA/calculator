977a90934b4c0ef67f913806f6686079e549e392
soniya <soniyakangujam188@gmail.comi>
2023-12-13 15:33:53 +0530

    calculator program in c

---
+++ calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+	int sum=0,diff=0,a,b,c,d;
+	printf("enter the nos to be added:");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("The sum of two nos is: ",sum);
+	printf("enter the nos to be subtract:");
+	scanf("%d%d",&c,&d);
+	diff=c-d;
+	printf("diff of two nos is: ",diff);
